ae377a6dc0382c7cf77cf09e8062347983984b8f
Thiago Parisotto Dias <thiagodias158@hotmail.com>
2023-12-13 15:09:04 -0300

    etapa4

---
+++ Entregaveis/etapa3/hash.h
@@ -0,0 +1,24 @@
+//Thiago Parisotto Dias Tabela Hash
+//Utilizada a implementação feita pelo professor durante as aulas com algumas leves modificações
+#ifndef HASH_HEADER
+#define HASH_HEADER
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HASH_SIZE 997
+
+typedef struct hash_node
+{
+    int type;
+    char *text;
+    struct hash_node *next;
+} HASH_NODE;
+
+void hashInit(void);
+int hashAddress(char *text);
+HASH_NODE *hashInsert(int type, char *text);
+void hashPrint(void);
+
+#endif
+//END
---
+++ Entregaveis/etapa3/main.c
@@ -0,0 +1,46 @@
+// Thiago Parisotto Dias
+#include <stdio.h>
+#include <stdlib.h>
+#include "ast.h"
+extern FILE* yyin;
+extern int isRunning();
+extern int getLineNumber();
+extern int yyparse();
+extern void initMe();
+extern void hashPrint();
+extern AST* getRoot();
+
+int main(int argc, char **argv) {
+    FILE* output;
+    //se não tiver 2 args
+    if (argc <= 2) {
+        fprintf(stderr, "Call: ./etapa3 input.txt output.txt\n");
+        exit(1);
+    }
+    //abertura de arquivo para leitura
+    if (0 == (yyin = fopen(argv[1],"r"))) {
+        fprintf(stderr, "Cannot open file %s\n", argv[1]);
+        exit(2);
+    }
+    //abertura de arquivo para escrita
+    if (0 == (output = fopen(argv[2],"w"))) {
+        fprintf(stderr, "Cannot open file %s\n", argv[2]);
+        exit(2);
+    }
+    //inicialização
+    initMe();
+
+    yyparse();
+
+    fprintf(stderr, "Parsing done!\n");
+    
+    uncompile(getRoot(), output);
+
+    fprintf(stderr, "Uncompiling done!\n");
+
+    printf("Sucesso!\n");
+
+
+    exit(0);
+}
+
